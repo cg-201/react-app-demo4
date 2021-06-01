e77794ae699c21fb2dddb4f62b90115c7a206bf0
santosh <santosh.ece06@gmail.com>
2021-06-01 17:37:55 +0530

    routing done with navitatoin and Link tag.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,42 @@ import { Page1 } from "./pages/Page1";
 import { Page2 } from "./pages/Page2";
 import { Page3 } from "./pages/Page3";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 function App() {
   return (
     <Router>
       {/** NAVIGATION LOGIC */}
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Project Book</Navbar.Brand>
+      <Navbar bg="dark" expand="lg" variant="dark" className="justify-between">
+        <Link to="/">
+          <Navbar.Brand href="#home">Project Book</Navbar.Brand>
+        </Link>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/page1">
+              <Nav.Link href="#home">Page1</Nav.Link>
+            </Link>
+
+            <Link to="/page2">
+              <Nav.Link href="#link">Page2</Nav.Link>
+            </Link>
+
+            <Link to="/page3">
+              <Nav.Link href="#link">Page3</Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
 
       <Switch>
